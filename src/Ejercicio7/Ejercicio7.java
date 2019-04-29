16c1f7af5f67f3bc83ca277cc7f05eaf1fa73854
Esther <esthertarazona81@gmail.com>
2019-04-29 17:41:46 +0200

    fin

---
+++ src/Ejercicio7/Ejercicio7.java
@@ -0,0 +1,79 @@
+package Ejercicio7;
+import java.util.*;
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Realizar un programa que guarde en un map el nombre de usuario asociado
+		 * a un objeto Persona(Puedes usar clase desarrollada en ejercicios anteriores).
+		 * Este mapa debe poder añadir nuevos usuarios, eliminar un usuariosegun una clave 
+		 * leida por teclado y modificar un usuario en caso de que este presente en el mapa.
+		 * */
+		 
+		Scanner teclado= new Scanner(System.in);
+		
+		//declaramos el map
+		Map<String,Persona> m= new TreeMap();
+		//insertamos 3 personas en el map
+		Persona p1= new Persona("Paqui","garciacarricondo@iestubalcain.net", "user1","12345678");
+		Persona p2= new Persona("Esther","esther.lainez.lainez@iestubalcain.net", "user2","12345678");
+		Persona p3= new Persona("Jorge","jorgerivas@iestubalcain.net", "user3","12345678");
+		
+		m.put("pCarricondo", p1);
+		m.put("eLinez", p2);
+		m.put("jRivas", p3);
+		System.out.println(m);
+		int opcion;
+		do {
+			System.out.println("Menu");
+			System.out.println("1. Inicializar el map");
+			System.out.println("2. Insertar nuevo elemento en map");
+			System.out.println("3.Modificar en map");
+			System.out.println("4.Borrar en map");
+			System.out.println("5.Salir");
+			System.out.println("Introducir opcion: ");
+			 opcion=teclado.nextInt();
+			switch(opcion) {
+			
+			case 1:
+				Persona p4= new Persona("Javi","falceshernandez@iestubalcain.net", "user4","12345678");
+				m.put("jFalces", p4);
+				System.out.println(m);
+				break;
+			case 2:
+				/*1)Leemos por teclado la clave que queremos modificar
+				 * 2) Si existe
+				 * m.put(objeto con campo usuario modificado);
+				 * 3)enviamos mensaje
+				 */
+				System.out.println("Introduzca la clave que quiere modificar");
+				String claveModificar= teclado.nextLine();
+				if(m.containsKey(claveModificar)){
+					Persona p= m.get(claveModificar);
+					System.out.println("Introduzca el nuevo usuario que quiere modificar");
+					String nuevoUser= teclado.nextLine();
+					p.setUsuario(nuevoUser);
+					m.put(claveModificar, p);
+					System.out.println(m);
+				}else {
+					System.out.println("Este usuario no existe");
+				}
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+				
+			case 5:
+				break;
+			
+			}
+			
+			
+			
+			
+			
+		}while (opcion!=5);
+	}
+
+}
---
+++ src/Ejercicio7/Persona.java
@@ -0,0 +1,50 @@
+package Ejercicio7;
+
+public class Persona {
+	String nombre;
+	String apellidos;
+	String email;
+	String usuario;
+	String contraseña;
+	
+	public Persona(String nombre, String email, String usuario, String contraseña) {
+		super();
+		this.nombre = nombre;
+		this.email = email;
+		this.usuario = usuario;
+		this.contraseña = contraseña;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellidos=" + apellidos + ", email=" + email + ", usuario=" + usuario
+				+ ", contraseña=" + contraseña + "]\n";
+	}
+
+}
